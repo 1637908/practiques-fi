d3565a7a328d8b89003a1c07fa77ca9b82c1144e
1637908 <1637908@uab.cat>
2021-10-04 11:15:03 +0200

    tercera  modificació (eliminació columnes+comentari)

--- practica.sh
+++ practica.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
+#eliminem columnes 12 i 16 de l'arxiu CAvideos.csv i dirigim el resultat al l'arxiu CAvideos_primeramodificació.csv
 cut -d, -f 1,2,3,4,5,6,7,8,9,10,11,13,14,15 CAvideos.csv > CAvideos_primeramodificació.csv
